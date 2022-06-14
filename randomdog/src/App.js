c3d20c52b0dd2a71b3a9383766aa774a671035a0
David Aydin <david.aydin6@gmail.com>
2022-06-13 21:45:19 -0400

    useEffect()

--- randomdog/src/App.js
+++ randomdog/src/App.js
@@ -1,20 +1,25 @@
-import {useState} from "react";
+import { useState, useEffect } from "react";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-
   const apiKey = "https://dog.ceo/api/breeds/image/random";
   const [dog, setDog] = useState(null);
+
   const getDog = async () => {
     const response = await fetch(`${apiKey}`);
     const dogData = await response.json();
     setDog(dogData);
   };
 
+  useEffect(() => {
+    getDog();
+  }, []);
 
   return (
-    <div className="App"></div>
+    <div className="App">
+      <h1>Hello World!</h1>
+    </div>
   );
 }
 
